15fa284788ac5843369e43e71bc2e3a303196d05
Sinha <RLS34420@CHMCA.ORG>
2018-08-20 14:21:18 -0400

    first commit

---
+++ SignalR-Hub/Controllers/MessageController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SignalRHub
+{
+    [Route("api/[controller]")]
+    public class MessageController : Controller
+    {
+
+        //referencing NotifyHub & ITypedHubClient using SignalR IHubContext
+        private IHubContext<NotifyHub, ITypedHubClient> _hubContext;
+
+        public MessageController(IHubContext<NotifyHub, ITypedHubClient> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+       
+        [HttpPost]
+        public string Post([FromBody]Message msg)
+        {
+            string retMessage = string.Empty;
+            try
+            {
+                _hubContext.Clients.All.BroadcastMessage(msg.Type, msg.Payload);
+                retMessage = "Notification Sent Successfully";
+            }
+            catch (Exception e)
+            {
+                retMessage = e.ToString();
+            }
+            return retMessage;
+        }
+    }
+}
